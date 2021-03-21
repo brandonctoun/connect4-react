582fc5c3c9177ad6e210ee064c49609d9ed22f07
Brandon C Toun <brandonctoun@hotmail.com>
2021-03-21 04:43:54 -0400

    remove unused css and web vitals

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <h1>Connect 4</h1>
       <Board />
-      <div><small>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></small></div>
     </div>
   );
 }
